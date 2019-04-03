9d75521d277da63621495aec5996f033a350db22
it-school <it@osvita.od.ua>
2019-04-03 21:37:48 +0300

    v.1

--- DelegatesGUI/Form1.Designer.cs
+++ DelegatesGUI/Form1.Designer.cs
@@ -28,13 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(186, 261);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(267, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(468, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "   Text ,      hello !hi   ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(513, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.checkBox3);
+            this.panel1.Controls.Add(this.checkBox2);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Location = new System.Drawing.Point(27, 21);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(196, 177);
+            this.panel1.TabIndex = 4;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(16, 14);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(127, 21);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "multiple spaces";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(16, 52);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(166, 21);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "spaces before puncts";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(16, 96);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(154, 21);
+            this.checkBox3.TabIndex = 7;
+            this.checkBox3.Text = "spaces after puncts";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- DelegatesGUI/Form1.cs
+++ DelegatesGUI/Form1.cs
@@ -12,9 +12,71 @@ namespace DelegatesGUI
 {
     public partial class Form1 : Form
     {
+        static string text;
+        delegate void Modify();
+        Modify op1;
+        Modify op2;
+        Modify op3;
+
         public Form1()
         {
+            text = "";
             InitializeComponent();
+            op1 = MultipleSpaces;
+            op2 = RemoveSpacesBeforePuncts;
+            op3 = InsertSpacesAfterPuncts;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Modify res = new Modify(Start);
+            text = textBox1.Text;
+            if (checkBox1.Checked)
+                res += op1;
+            if (checkBox2.Checked)
+                res += op2;
+            if (checkBox3.Checked)
+                res += op3;
+
+
+            res();
+            textBox1.Text = text;
+
+        }
+
+        // Организуем ряд методов
+        static void MultipleSpaces()
+        {
+            text = text.Trim();
+            while (text.IndexOf("  ") >= 0)
+            {
+                 text = text.Replace("  ", " ");
+            }
+        }
+
+        static void RemoveSpacesBeforePuncts()
+        {
+            text = text.Trim();
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (Char.IsPunctuation(text[i]) == true && text[i - 1] == ' ')
+                    text = text.Remove(i - 1, 1);
+            }
+        }
+
+        static void InsertSpacesAfterPuncts()
+        {
+            text = text.Trim();
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (Char.IsPunctuation(text[i]) == true && text[i + 1] != ' ')
+                    text = text.Insert(i + 1, " ");
+               
+            }
+            int a = 0;
+        }
+        static void Start()
+        {
         }
     }
 }
